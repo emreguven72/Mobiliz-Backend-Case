d99bbe46551a0390cbe64b9b0b9bc73ba77fa856
emreguven72 <emreguven_72@hotmail.com>
2023-08-08 21:43:47 +0300

    Project Started

---
+++ src/main/java/com/emregvn/mobilizbackendcase/MobilizBackendCaseApplication.java
@@ -0,0 +1,13 @@
+package com.emregvn.mobilizbackendcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilizBackendCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilizBackendCaseApplication.class, args);
+	}
+
+}
